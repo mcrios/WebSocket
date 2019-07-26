35c5e3430603e995326a9c0523a3932480469ace
mcrios <mauro99rios@gmail.com>
2019-07-26 16:23:00 -0600

    WebSocket Con Spring Boot Chat de prueba aun falta implementar almacenar chat en BD

---
+++ src/main/java/com/datasoft/chat/controller/ChatController.java
@@ -0,0 +1,27 @@
+package com.datasoft.chat.controller;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import com.datasoft.chat.model.ChatMessage;
+
+@Controller
+public class ChatController {
+
+	@MessageMapping("/chat/{room}/register")
+	@SendTo("/topic/{room}")
+	public ChatMessage register(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+		headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+		return chatMessage;
+	}
+	
+	@MessageMapping("/chat/{room}/send")
+	@SendTo("/topic/{room}")
+	public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+		return chatMessage;
+	}
+
+}
---
+++ src/main/java/com/datasoft/chat/controller/WebSocketListener.java
@@ -0,0 +1,50 @@
+package com.datasoft.chat.controller;
+
+import static java.lang.String.format;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+import com.datasoft.chat.model.ChatMessage;
+import com.datasoft.chat.model.ChatMessage.MessageType;
+
+
+@Component
+public class WebSocketListener {
+
+	private static final Logger logger = LoggerFactory.getLogger(WebSocketListener.class);
+
+	@Autowired
+	private SimpMessageSendingOperations messagingTemplate;
+
+	@EventListener
+	public void handleWebSocketConnectListener(SessionConnectEvent event) {
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+		String username = (String) headerAccessor.getSessionAttributes().get("username");
+		logger.info(username + " Se ha unido al chat");
+	}
+
+	@EventListener
+	public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
+		StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+
+		String username = (String) headerAccessor.getSessionAttributes().get("username");
+		String roomId = (String) headerAccessor.getSessionAttributes().get("room_id");
+		if (username != null) {
+			logger.info(username + " Abandono el chat");
+
+			ChatMessage chatMessage = new ChatMessage();
+			chatMessage.setType(MessageType.LEAVE);
+			chatMessage.setSender(username);
+
+			messagingTemplate.convertAndSend(format("/topic/%s", roomId), chatMessage);
+		}
+	}
+}
